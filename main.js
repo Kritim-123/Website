4f612263fa8bd4d32e353df86150f28422ec1a1a
Kritim-123 <kritimbastola2005@gmail.com>
2026-06-09 16:36:23 -0600

    Change to the structure of the website

--- main.js
+++ main.js
@@ -1,54 +1,47 @@
-// Handling navbar on small screen
-var sidemenu = document.getElementById("navbar");
+const sidemenu = document.getElementById("navbar");
 
 function openmenu() {
+  if (!sidemenu) return;
   sidemenu.classList.remove("sidemenu-close");
   sidemenu.classList.add("sidemenu-open");
 }
 
 function closemenu() {
+  if (!sidemenu) return;
   sidemenu.classList.remove("sidemenu-open");
   sidemenu.classList.add("sidemenu-close");
 }
 
-// Sending contact form data
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbydvFuqmCPakaOx9S3xlj2pX9LsochPp8akf3RYjEtvcrU5d2twvJAvLwPrEvmM1hCF/exec";
 const form = document.forms["submit-to-google-sheet"];
 const msg = document.getElementById("msg");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const loader = document.getElementById("loader");
-  const submit_text = document.getElementsByClassName("submit-txt")[0];
-  loader.classList.add("block");
-  submit_text.classList.add("hidden");
+if (form) {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const loader = document.getElementById("loader");
+    const submitText = document.getElementsByClassName("submit-txt")[0];
+    loader.classList.add("block");
+    submitText.classList.add("hidden");
+
     fetch(scriptURL, { method: "POST", body: new FormData(form) })
-      .then((response) => {
-        msg.classList.toggle('hidden')
+      .then(() => {
+        msg.classList.remove("hidden");
         msg.innerHTML = "Message sent successfully";
-        setTimeout(function () {
+        setTimeout(() => {
           msg.innerHTML = "";
+          msg.classList.add("hidden");
         }, 5000);
 
         form.reset();
-        loader.classList.toggle("block");
-        submit_text.classList.toggle("hidden");
+        loader.classList.remove("block");
+        submitText.classList.remove("hidden");
       })
-      .catch((error) => console.error("Error!", error.message));
-});
-
-// Handling About Tabs
-function opentab(event, tabname) {
-  const newActiveTab = document.getElementById(tabname);
-  const currentActiveTab = document.querySelector(".tab-links.active-link");
-  const currentActiveContent = document.querySelector(
-    ".tab-contents.active-tab"
-  );
-
-  currentActiveTab.classList.remove("active-link");
-  currentActiveContent.classList.remove("active-tab");
-
-  event.currentTarget.classList.add("active-link");
-  newActiveTab.classList.add("active-tab");
+      .catch((error) => {
+        console.error("Error!", error.message);
+        loader.classList.remove("block");
+        submitText.classList.remove("hidden");
+      });
+  });
 }
